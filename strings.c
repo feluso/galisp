9a5e04a236314a9edaaa476e876d17eaf5c23aaf
feluso <feluso2@gmail.com>
2019-02-17 19:27:54 -0500

    debugged and fixed load bug

--- strings.c
+++ strings.c
@@ -935,14 +935,14 @@ void lenv_add_builtins(lenv* e) {
 
 int main(int argc, char** argv) {
   /* Create some parses */ 
-  mpc_parser_t* Number = mpc_new("number");
-  mpc_parser_t* Symbol = mpc_new("symbol");
-  mpc_parser_t* String = mpc_new("string");
-  mpc_parser_t* Comment = mpc_new("comment");
-  mpc_parser_t* Sexpr = mpc_new("sexpr");
-  mpc_parser_t* Qexpr = mpc_new("qexpr");
-  mpc_parser_t* Expr = mpc_new("expr");
-  mpc_parser_t* Galisp = mpc_new("galisp");
+  Number = mpc_new("number");
+  Symbol = mpc_new("symbol");
+  String = mpc_new("string");
+  Comment = mpc_new("comment");
+  Sexpr = mpc_new("sexpr");
+  Qexpr = mpc_new("qexpr");
+  Expr = mpc_new("expr");
+  Galisp = mpc_new("galisp");
 	
   /* Define them with the following language */
   mpca_lang(MPCA_LANG_DEFAULT, 
